917bdfe06934602e81986b961f0b41dbecc654fc
chiranth <chiranthbr@gmail.com>
2014-03-29 12:08:38 +0530

    ScriptsWebAPi
    ScriptsWebAPi

---
+++ Chiranth/ScriptAPI/ScriptAPI/Controllers/ScriptController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using ScriptAPI.Models;
+
+namespace ScriptAPI.Controllers
+{
+    public class ScriptController : ApiController
+    {
+        private ScriptDBContext db = new ScriptDBContext();
+
+        // GET api/Script
+        public IEnumerable<Script> GetScripts()
+        {
+            return db.Scripts.AsEnumerable();
+        }
+
+        // GET api/Script/5
+        public Script GetScript(int id)
+        {
+            int SID = id;
+            Script script = db.Scripts.Find(SID);
+            if (script == null)
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+
+            return script;
+        }
+
+        // PUT api/Script/5
+        public HttpResponseMessage PutScript(int id, Script script)
+        {
+            if (ModelState.IsValid && id == script.SID)
+            {
+                db.Entry(script).State = EntityState.Modified;
+
+                try
+                {
+                    db.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound);
+                }
+
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+
+        // Get api/Script?sname="value"
+        public IEnumerable<Script> GetScriptbyname(string sname)
+        {
+            var scripts = from m in db.Scripts select m;
+
+            if (!String.IsNullOrEmpty(sname))
+            {
+                scripts = scripts.Where(s => s.SName.Contains(sname));
+            }
+
+            return scripts.AsEnumerable();
+
+        }
+
+        // POST api/Script
+        public HttpResponseMessage PostScript(Script script)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Scripts.Add(script);
+                db.SaveChanges();
+
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, script);
+                response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = script.SID }));
+                return response;
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+
+        // DELETE api/Script/5
+        public HttpResponseMessage DeleteScript(int id)
+        {
+            Script script = db.Scripts.Find(id);
+            if (script == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            db.Scripts.Remove(script);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, script);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Chiranth/ScriptAPI/ScriptAPI/Models/Script.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ScriptAPI.Models
+{
+    public class Script
+    {
+        [Key]
+        public int SID { get; set; }
+        public string SName { get; set; }
+        public string SAuthor { get; set; }
+        public string SType { get; set; }
+        [DataType(DataType.MultilineText)]
+        public string SData { get; set; }
+        public decimal Sversion { get; set; }
+        public DateTime UploadDate { get; set; }
+    }
+
+    public class ScriptDBContext : DbContext
+    {
+        public DbSet<Script> Scripts { get; set; }
+        //public DbSet<ScriptData> ScriptssData { get; set; }
+    }
+}
